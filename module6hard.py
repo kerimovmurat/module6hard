4b9ee0c7060309f54f88213f3d9010f352eb70ff
kerimovmurat <kerimovmurat@yandex.ru>
2024-09-10 14:08:37 +0300

    module6hard.py

--- module6hard.py
+++ module6hard.py
@@ -1,83 +1,68 @@
-class Figure:
+class Figure():
     sides_count = 0
-
-    def __init__(self, color, *sides):
-        self.__color = list(color)
-        self.__sides = list(sides) if len(sides) == self.sides_count else [1] * self.sides_count
-        self.filled = False
+    def __init__(self, color, sides, filled = True):
+        self.sides = sides
+        self.color = color
 
     def get_color(self):
-        return self.__color
+        return self.color
 
-    def __is_valid_color(self, r, g, b):
-        return all(isinstance(i, int) and 0 <= i <= 255 for i in (r, g, b))
+    def is_valid_color(self, r, g, b):
+
+        if type(r) == int and type(g) == int and type(b) == int:
+            if 0 <= r <= 255 and 0 <= g <= 255 and 0 <= b <= 255:
+                return True
+        return False
 
     def set_color(self, r, g, b):
-        if self.__is_valid_color(r, g, b):
-            self.__color = [r, g, b]
+        if self.is_valid_color(r, g, b):
+            self.color = list((r, g, b))
 
-    def __is_valid_sides(self, *new_sides):
-        return all(isinstance(side, int) and side > 0 for side in new_sides) and len(new_sides) == self.sides_count
+    def is_valid_sides(self, *sides):
+        return all(isinstance(side, int) and side > 0 for side in sides) and len(sides) == self.sides_count
 
     def get_sides(self):
-        return self.__sides
+        return self.sides_list
 
     def __len__(self):
-        return sum(self.__sides)
+        return sum(self.sides_list)
 
     def set_sides(self, *new_sides):
-        if self.__is_valid_sides(*new_sides):
-            self.__sides = list(new_sides)
+        if self.is_valid_sides(*new_sides):
+            self.sides_count = len(new_sides)
+            self.sides_list = list(new_sides)
+
+
+
 
 class Circle(Figure):
     sides_count = 1
-
-    def __init__(self, color, *sides):
-        super().__init__(color, *sides)
-        self.__radius = self.get_sides()[0] / (2 * 3.14159)
+    def __init__(self, color, side, filled = True):
+        super().__init__(color, side)
+        self.set_color(color[0], color[1], color[2])
+        self.set_sides(side)
+        self.radius = side/ 2 * 3.14
 
     def get_square(self):
-        return 3.14159 * (self.__radius ** 2)
+        return 3.14 * self.radius ** 2
 
 class Triangle(Figure):
     sides_count = 3
+    def __init__(self, color, sides, filled = True):
+        super().__init__(color, sides)
+        self.set_color(color[0], color[1], color[2])
+        self.set_sides(sides)
 
     def get_square(self):
-        a, b, c = self.get_sides()
-        s = (a + b + c) / 2
-        return (s * (s - a) * (s - b) * (s - c)) ** 0.5
+        p = len(self) / 2
+        return (p * (p - self.sides[0])* (p - self.sides[1])* (p - self.sides[2])) ** 0.5
+
 
 class Cube(Figure):
     sides_count = 12
-
-    def __init__(self, color, *sides):
-        super().__init__(color, *sides)
-        if len(sides) == 1:
-            self.set_sides(*([sides[0]] * self.sides_count))
-        else:
-            self.set_sides(*([1] * self.sides_count))
-
+    def __init__(self, color, sides, filled = True):
+        super().__init__(color, sides)
+        self.set_color(color[0], color[1], color[2])
+        self.set_sides(*([sides]*self.sides_count))
     def get_volume(self):
-        return self.get_sides()[0] ** 3
-
-# Пример использования:
-circle1 = Circle((200, 200, 100), 10)
-cube1 = Cube((222, 35, 130), 6)
-
-# Проверка на изменение цветов:
-circle1.set_color(55, 66, 77)
-print(circle1.get_color())
-cube1.set_color(300, 70, 15)
-print(cube1.get_color())
-
-# Проверка на изменение сторон:
-cube1.set_sides(5, 3, 12, 4, 5)
-print(cube1.get_sides())
-circle1.set_sides(15)
-print(circle1.get_sides())
-
-# Проверка периметра (круга), это и есть длина:
-print(len(circle1))
-
-# Проверка объёма (куба):
-print(cube1.get_volume())
+        return self.sides_list[0] ** 3
